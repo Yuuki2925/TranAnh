126a3b28efeb6ef7834ffb0b80c8dbdeb8a7facf
Yuuki2925 <tanh15296@gmail.com>
2024-11-15 14:01:33 +0700

    Update

---
+++ Module 1/If-else Statement/Check Leap Year.js
@@ -0,0 +1,13 @@
+function LeapYearr(){
+    let year = Number(document.getElementById("leapYear").value);
+
+    if((year % 4 == 0 && year % 100 != 0) || (year % 100 == 0 && year % 400 == 0)){
+        document.getElementById("result").innerHTML = "Đây là năm nhuận";
+    }
+    else if(year % 100 == 0 && year % 400 != 0){
+        document.getElementById("result").innerHTML = "Đây không phải năm nhuận";
+    }
+    else {
+        document.getElementById("result").innerHTML = "Đây không phải năm nhuận";
+    }
+}
