9ab8104f47b4ab721cf9950540b27029204607d4
Yuuki2925 <tanh15296@gmail.com>
2025-02-12 19:37:19 +0700

    Update

---
+++ Module_2/SortAlgorithm/InsertionSort/InsertionSort.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class InsertionSort {
+    public static void insertionSort(int[] array) {
+        
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your array size: ");
+        int size = scanner.nextInt();
+        int[] array = new int[size];
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.print("arr[" + i + "] = ");
+            array[i] = scanner.nextInt();
+        }
+    }
+}
