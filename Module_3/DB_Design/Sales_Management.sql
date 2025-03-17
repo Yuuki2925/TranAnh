0c4e6092e6d99f10134da516a5c60ceb32143c94
Yuuki2925 <tanh15296@gmail.com>
2025-03-17 19:39:15 +0700

    Upload

--- Module_3/DB_Design/Sales_Management.sql
+++ Module_3/DB_Design/Sales_Management.sql
@@ -30,59 +30,6 @@ CREATE TABLE OrderDetail (
     CONSTRAINT FK_P_OD FOREIGN KEY (pID) REFERENCES Product(pID)
 );
 
-INSERT INTO Customer (cID, cName, cAge)
-VALUES
-(1, 'Minh Quan', 10),
-(2, 'Ngoc Oanh', 20),
-(3, 'Hong Ha', 50);
-
-INSERT INTO `Order` (oID, cID, oDate, oTotalPrice)
-VALUES
-(1, 1, '2006-3-21', NULL),
-(2, 2, '2006-3-23)', NULL), 
-(3, 1, '2006-3-16', NULL);
-
-INSERT INTO Product (pID, pName, pPrice)
-VALUES
-(1, 'May Giat', 3),
-(2, 'Tu Lanh', 5),
-(3, 'Dieu Hoa', 7),
-(4, 'Quat', 1),
-(5, 'Bep Dien', 2);
-
-INSERT INTO OrderDetail (oID, pID, odQTY)
-VALUES
-(1, 1, 3),
-(1, 3, 7),
-(1, 4, 2),
-(2, 1, 1),
-(3, 1, 8),
-(2, 5, 4),
-(2, 3, 3);
-
--- Hiển thị các thông tin gồm oID, oDate, oPrice của tất cả các hóa đơn trong bảng Order
-SELECT oID, oDate
-FROM `Order`;
-
--- Hiển thị danh sách các khách hàng đã mua hàng, và danh sách sản phẩm được mua bởi các khách
-SELECT Customer.cName, Product.pName, Product.pPrice
-FROM Customer JOIN `Order` ON Customer.cID = `Order`.cID
-			  JOIN OrderDetail ON `Order`.oID = OrderDetail.oID
-			  JOIN Product ON OrderDetail.pID = Product.pID;
-
--- Hiển thị tên những khách hàng không mua bất kỳ một sản phẩm nào
-SELECT cName
-FROM Customer
-WHERE cID NOT IN (SELECT DISTINCT cID
-				  FROM `Order`);
-
--- Hiển thị mã hóa đơn, ngày bán và giá tiền của từng hóa đơn 
--- (giá một hóa đơn được tính bằng tổng giá bán của từng loại mặt hàng xuất hiện trong hóa đơn. 
--- Giá bán của từng loại được tính = odQTY*pPrice)
-SELECT `Order`.oID, `Order`.oDate, sum(odQTy * pPrice) as GiaBan
-FROM `Order` JOIN OrderDetail ON `Order`.oID = OrderDetail.oID
-			 JOIN Product ON OrderDetail.pID = Product.pID
-Group by `Order`.oID, `Order`.oDate
 
 
 
